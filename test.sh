2462efb87411d64e40a720345202bb094100bb9e
Raspurrin <vondetou@gmail.com>
2022-11-02 02:36:49 +0100

    Reworked the tester

--- test.sh
+++ test.sh
@@ -41,7 +41,7 @@ while getopts "d123456789abc" opt; do
 			DESC5="test5: ${VIOLET}(${BLACK}export something=blue; env ${VIOLET}>outfile1)${NC}\\n"
 			DESC6="test6: ${VIOLET}(${BLACK}export something=;env ${VIOLET}>outfile1 ${BLACK};export ${VIOLET}>outfile2;${BLACK}unset something; export ${VIOLET}>outfile3):${NC}\\n"
 			DESC7="test7: ${VIOLET}(${BLACK}export something=====blue;env ${VIOLET}>outfile1;):${NC}\\n"
-			DESC8="test8: ${VIOLET}(${BLACK}echo -nnn -----nn --nnnnn >outfile1):${NC}\\n"
+			DESC8="test8: ${VIOLET}(${BLACK}echo -nnn -----nn --nnnnn ${VIOLET}>outfile1):${NC}\\n"
 			DESC9="test9: ${VIOLET}(${BLACK}env env env ${VIOLET}>outfile1):${NC}\\n"
 			DESC10="test10: ${VIOLET}(${BLACK}env env hoi ${VIOLET}>outfile1):${NC}\\n"
 			DESC11="test11: ${VIOLET}(${BLACK}unset PWD; env | grep PWD ${VIOLET}>outfile1):${NC}\\n"
@@ -102,9 +102,9 @@ while getopts "d123456789abc" opt; do
 			./minishell 6
 			export something=;env >test1;export >test2;unset something; export >test3
 			if diff -a ${FOLDER6}outfile3 ${FOLDER6}test3 > ${FOLDER6}diff ; then
-				TEST6="${DESC5} ${GREEN}[OK]${NC}\\n"
+				TEST6="${DESC6} ${GREEN}[OK]${NC}\\n"
 			else 
-				TEST6="${DESC5} ${RED}[nope]${NC}\\n"
+				TEST6="${DESC6} ${RED}[nope]${NC}\\n"
 			fi
 			;;
 		7)
@@ -112,61 +112,63 @@ while getopts "d123456789abc" opt; do
 			./minishell 7
 			export something=====blue;env >test1;
 			if diff -a ${FOLDER7}outfile1 ${FOLDER7}test1 > ${FOLDER7}diff ; then
-				TEST7="${DESC5} ${GREEN}[OK]${NC}\\n"
+				TEST7="${DESC7} ${GREEN}[OK]${NC}\\n"
 			else 
-				TEST7="${DESC5} ${RED}[nope]${NC}\\n"
+				TEST7="${DESC7} ${RED}[nope]${NC}\\n"
 			fi
 			;;
 		8)
 			rm -f ${FOLDER8}outfile1 ${FOLDER8}test1
 			./minishell 8
-			 echo -nnn -----nn --nnnnn >test1
+			 echo -nnn -----nn --nnnnn > ${FOLDER8}test1
 			if diff -a ${FOLDER8}outfile1 ${FOLDER8}test1 > ${FOLDER8}diff ; then
-				TEST7="${DESC5} ${GREEN}[OK]${NC}\\n"
+				TEST8="${DESC8} ${GREEN}[OK]${NC}\\n"
 			else 
-				TEST7="${DESC5} ${RED}[nope]${NC}\\n"
+				TEST8="${DESC8} ${RED}[nope]${NC}\\n"
 			fi
 			;;
 		9)
 			rm -f ${FOLDER9}outfile1 ${FOLDER9}test1
 			./minishell 9
-			env env env >test1
+			env env env > ${FOLDER9}test1
 			if diff -a ${FOLDER9}outfile1 ${FOLDER9}test1 > ${FOLDER9}diff ; then
-				TEST7="${DESC5} ${GREEN}[OK]${NC}\\n"
+				TEST9="${DESC9} ${GREEN}[OK]${NC}\\n"
 			else 
-				TEST7="${DESC5} ${RED}[nope]${NC}\\n"
+				TEST9="${DESC9} ${RED}[nope]${NC}\\n"
 			fi
 			;;
-		10)
+		a)
 			rm -f ${FOLDER10}outfile1 ${FOLDER10}test1
 			./minishell 10
-			env env hoi >test1
+			env env hoi > ${FOLDER10}test1
 			if diff -a ${FOLDER10}outfile1 ${FOLDER10}test1 > ${FOLDER10}diff ; then
-				TEST7="${DESC5} ${GREEN}[OK]${NC}\\n"
+				TEST10="${DESC10} ${GREEN}[OK]${NC}\\n"
 			else 
-				TEST7="${DESC5} ${RED}[nope]${NC}\\n"
+				TEST10="${DESC10} ${RED}[nope]${NC}\\n"
 			fi
 			;;
-		11)
+		b)
 			rm -f ${FOLDER11}outfile1 ${FOLDER11}test1
 			./minishell 11
-			unset PWD; env | grep PWD >test1
+			unset PWD; env | grep PWD > ${FOLDER11}test1
 			if diff -a ${FOLDER11}outfile1 ${FOLDER11}test1 > ${FOLDER11}diff ; then
-				TEST5="${DESC5} ${GREEN}[OK]${NC}\\n"
+				TEST11="${DESC11} ${GREEN}[OK]${NC}\\n"
 			else 
-				TEST5="${DESC5} ${RED}[nope]${NC}\\n"
+				TEST11="${DESC11} ${RED}[nope]${NC}\\n"
 			fi
 			;;
-		12)
+		c)
 			rm -f ${FOLDER12}outfile1 ${FOLDER12}test1
 			./minishell 12
-			/bin/cd ../;pwd >test1
+			/bin/cd ../;pwd > ${FOLDER12}test1
 			if diff -a ${FOLDER12}outfile1 ${FOLDER12}test1 > ${FOLDER12}diff ; then
-				TEST5="${DESC5} ${GREEN}[OK]${NC}\\n"
+				TEST12="${DESC12} ${GREEN}[OK]${NC}\\n"
 			else 
-				TEST5="${DESC5} ${RED}[nope]${NC}\\n"
+				TEST12="${DESC12} ${RED}[nope]${NC}\\n"
 			fi
 			;;
 	esac
 done
-echo -e "${TEST1}${TEST2}${TEST3}${TEST4}${TEST5}" | awk '/./'
+echo -e "${TEST1}${TEST2}${TEST3}${TEST4}${TEST5}${TEST6}${TEST7}${TEST8}${TEST9}${TEST10}${TEST11}${TEST12}" | awk '/./'
+
+# Hanging: 4 6 b c
