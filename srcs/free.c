61616efb58397b72787b7fde30392b4a5585e24c
Raspurrin <vondetou@gmail.com>
2022-11-13 07:34:29 +0100

    Seperated free functions to its own file

---
+++ srcs/free.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 06:36:38 by mialbert          #+#    #+#             */
+/*   Updated: 2022/11/13 06:43:17 by mialbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+/**
+ * Not freeing keyvalue because it is directly taken from
+ * envp from main. 
+ */
+void	free_env_node(t_env *lst, bool free_all)
+{
+	if (!lst)
+		return ;
+	if (lst->keyvalue)
+		free(lst->keyvalue);
+	if (lst->key)
+		free(lst->key);
+	if (lst->value)
+		free(lst->value);
+	ft_bzero(lst, sizeof(lst));
+	if (lst && free_all == true)
+		free(lst);
+}
+
+void	free_env_lst(t_env *lst)
+{
+	t_env	*tmp;
+
+	if (!lst)
+		return ;
+	tmp = lst;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		free_env_node(lst, true);
+		lst = tmp;
+	}
+	tmp = NULL;
+}
+
+void	lst_clear(void *file_lst)
+{
+	t_infile	*lst;
+	t_infile	*tmp;
+
+	if(!file_lst)
+		return ;
+	tmp = (t_infile *)file_lst;
+	lst = tmp;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		if (lst->name)
+		{
+			free(lst->name);
+			lst->name = NULL;
+		}
+		if (lst)
+		{
+			free(lst);
+			lst = NULL;
+		}
+		lst = tmp;
+	}
+	tmp = NULL;
+}
+
+void	free_groups(t_data *data)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < data->groupc)
+	{
+		if (data->group[i].full_cmd)
+			free_2d(data->group[i].full_cmd);
+		if (data->group[i].outfile)
+			lst_clear((void *)data->group[i].outfile);
+		if (data->group[i].infile)
+			lst_clear((void *)data->group[i].infile);
+		i++;
+	}
+	free(data->group);
+	data->group = NULL;
+}
+
+void	free_data(t_data *data)
+{
+	free_2d(data->paths);
+	free_env_lst((void *)data->envp_head);
+	if (data->pwd)
+	{
+		free(data->pwd);
+		data->pwd = NULL;
+	}
+	free_groups(data);
+}
