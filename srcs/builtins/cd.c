a65caa33575a0f8a1b1f301211446d95a240b4c2
Raspurrin <vondetou@gmail.com>
2022-11-13 07:28:12 +0100

    Revamped absolute/relative path finding and made it modular

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,64 +6,87 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 03:22:30 by mialbert          #+#    #+#             */
-/*   Updated: 2022/11/08 07:51:17 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/11/13 05:44:28 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+/**
+ * if PWD is unset OLDPWD's value changes to ""
+ * Otherwise OLDPWD gets PWD's old value before I change PWD's value in cd.
+ */
 static void	update_oldpwd(t_data *data)
 {
-	t_env	*lst;
+	t_env	*oldpwd;
+	t_env	*pwd;
 
-	lst = find_node(data->envp_head, "OLDPWD");
-	lst->value = data->pwd;
-}
-
-static bool	check_error(t_group *group)
-{
-	if (!access(group->full_cmd[1], R_OK | F_OK))
-	{
-		ft_printf_fd(STDOUT_FILENO, "cd: no such file or direcotry: \
-											%s", group->full_cmd[1]);
-		return (false);
-	}
-	return (true);
+	oldpwd = find_node(data->envp_head, "OLDPWD");
+	pwd = find_node(data->envp_head, "PWD");
+	if (!pwd)
+		oldpwd->value = "";
+	else 
+		oldpwd->value = data->pwd;
 }
 
 /**
- * It seems like the environmental variable PWD doesn't change its value 
- * after using chdir(), which is why I am manually changing this string.
+ * @param relative The relative path to edit path with. Ex: ../../evals/ft_irc
+ * outcome: Documents/evals/ft_irc
+ * @param path The path you want to start with. 
+ * Ex: Documents/my_projects/minishell
+ */
+char	*relative_path(char *relative, char *path)
+{
+	size_t	len;
+	char	*folder;
+	
+	len = 0;
+	if (ft_strncmp(relative, "./", 2) == 0)
+		relative += 2;
+	while (ft_strncmp(relative, "../", 3) == 0)
+	{
+		len = pos_char_end(path, '/');
+		path = ft_substr(path, 0, len);
+		relative += 3;
+	}
+	while (relative)
+	{
+		len = pos_char_start(relative, '/');
+		folder = ft_substr(relative, 0, len);
+		path = ft_strjoin(path, folder);
+		relative += len;
+	}
+	if (access(path, F_OK | X_OK) == 0)
+		return (path);
+	return (NULL);
+}
+
+char	*absolute_or_relative(char *path, char *old_path)
+{
+	if (access(path, F_OK | X_OK) == 0)
+		return (path);
+	path = relative_path(path, old_path);
+	if (!path)
+		return (NULL);
+	return (path);
+}
+
+/**
+ * Chdir returns -1 if the path cannot be changed. It can accept relative paths. 
+ * Oldpwd will be changed first, then I check if the path already works,
+ * in case of an absolute path given, otherwise the relative path will be solved.
+ * If PWD is not unset, the value will be changed as well.
+ * data->pwd exists in case PWD doesn't, since pwd should still work.
 */
 void	cd(t_data *data, t_group *group)
 {
-	size_t	len;
 	t_env	*env;
 
-	if (!check_error(group))
-		return ;
+	if (chdir(group->full_cmd[1]) == -1)
+		return (perror(NULL));
+	update_oldpwd(data);
+	data->pwd = absolute_or_relative(group->full_cmd[1], data->pwd);
 	env = find_node(data->envp_head, "PWD");
-	chdir(group->full_cmd[1]);
-	if (data->pwd)
-		update_oldpwd(data);
-	if (ft_strncmp(group->full_cmd[1], "../", ft_strlen(group->full_cmd[1])) == 0) // what if ../owfaijfoewaifj
-	{
-		if (group->full_cmd[2] == NULL)
-		{
-			len = ft_strlen(data->pwd);
-			while (data->pwd[len] != '/')
-				len--;
-			printf("len: %zu\n", len);
-			if (len == 0)
-				data->pwd = "/";
-			else
-				data->pwd = ft_substr(data->pwd, 0, len);
-			env->value = data->pwd;
-		}
-	}
-	else
-	{
-		data->pwd = ft_strjoin(data->pwd, "/");
-		data->pwd = ft_strjoin(data->pwd, group->full_cmd[1]);
-	}
+	if (env)
+		env->value = data->pwd;
 }
