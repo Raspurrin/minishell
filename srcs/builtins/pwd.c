6d0f88b09980a1450c55bec7115ec4aaa2ede457
Raspurrin <vondetou@gmail.com>
2022-11-13 07:32:14 +0100

    if lst->value to if lst to prevent segv

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 03:16:49 by mialbert          #+#    #+#             */
-/*   Updated: 2022/11/09 20:33:10 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/11/13 05:03:42 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	init_pwd(t_data *data)
 	t_env	*lst;
 
 	lst = find_node(data->envp_head, "PWD");
-	if (lst->value)
+	if (lst)
 		data->pwd = ft_strdup(lst->value);
 	else
 		data->pwd = NULL;
@@ -27,7 +27,7 @@ void	init_pwd(t_data *data)
 void	pwd(t_data *data, t_group *group)
 {
 	(void)group;
-	if (!data->pwd)
-		init_pwd(data);
-	printf("%s\n", data->pwd);
+
+	ft_printf_fd(STDERR_FILENO, "lst->value from pwd: %s\n", data->pwd);
+	ft_printf_fd(STDOUT_FILENO, "%s\n", data->pwd);
 }
