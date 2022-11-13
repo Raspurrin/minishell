82fd18a33399076ed2bca6a77d1ec5f24d4b4a17
Raspurrin <vondetou@gmail.com>
2022-11-13 04:32:05 +0100

    Fixed issue with unsetting last node

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 20:10:21 by mialbert          #+#    #+#             */
-/*   Updated: 2022/11/12 01:01:21 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/11/13 01:45:57 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,22 @@ void	unset(t_data *data, t_group *group)
 	t_env	*prev;
 
 	i = 1;
-	
 	while (group->full_cmd[i])
 	{
 		lst = data->envp_head;
 		prev = lst;
-		while (lst->next != NULL)
+		while (lst != NULL)
 		{
 			if (ft_strncmp(group->full_cmd[i], lst->key, ft_strlen(lst->key)) == 0 \
 			&& !ft_strncmp(group->full_cmd[i], lst->key, ft_strlen(group->full_cmd[i])))
 			{
-				if (lst == data->envp_head)
-					lst->next = data->envp_head;
+				if (lst->next == NULL)
+					lst = NULL;
+				else if (lst == data->envp_head)
+					data->envp_head = lst->next;
 				else
 					prev->next = lst->next;
-				free_env_node(lst, true);
+				free_env_node(lst, 1);
 				break ;
 			}
 			prev = lst;
