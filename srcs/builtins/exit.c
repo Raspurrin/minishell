434cd5adc2c4bee181140a1f83ed9b8b6dc14d9f
Djaisin <vondetou@gmail.com>
2022-11-25 22:49:43 +0100

    Normed exit.c

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -12,18 +12,29 @@
 
 #include "../../includes/minishell.h"
 
-
-
-uint32_t	exit_check(t_data *data, t_group *group)
-{	
-	size_t		i;
-	size_t		len;
-	uint32_t	nbr;
-	bool		errno;
+bool	check_digits(t_group *group)
+{
+	size_t	i;
+	size_t	len;
 
 	i = 0;
+	len = ft_strlen(group->full_cmd[1]);
+	if (len > 10 && ft_strncmp(group->full_cmd[1], ft_itoa(INT_MAX), len))
+		return (ft_atoi(group->full_cmd[1]));
+	while (ft_isdigit(group->full_cmd[1][i]))
+		i++;
+	if (group->full_cmd[1][i] && !ft_isdigit(group->full_cmd[1][i]))
+		return (false);
+	return (true);
+}
+
+uint32_t	exit_check(t_data *data, t_group *group)
+{
+	uint32_t	nbr;
+	bool		digits;
+
 	nbr = 0;
-	errno = false;
+	digits = true;
 	(void)data;
 	if (group->full_cmd[1] && group->full_cmd[2])
 	{
@@ -33,18 +44,12 @@ uint32_t	exit_check(t_data *data, t_group *group)
 	}
 	if (group->full_cmd[1])
 	{
-		len = ft_strlen(group->full_cmd[1]);
-		if (len > 10 && ft_strncmp(group->full_cmd[1], ft_itoa(INT_MAX), len))
-			return (ft_atoi(group->full_cmd[1]));
-		while (ft_isdigit(group->full_cmd[1][i]))
-			i++;
-		if (group->full_cmd[1][i] && !ft_isdigit(group->full_cmd[1][i]))
-			errno = true;
+		digits = check_digits(group);
 		nbr = ft_atoi(group->full_cmd[1]);
 	}
 	printf("exit\n");
 	shlvl_msg(data->envp_head, true);
-	if (errno)
+	if (!digits)
 		display_error(NUMARG, join_err(NULL, NULL), NULL, group);
 	g_exitcode = nbr;
 	exit(nbr);
