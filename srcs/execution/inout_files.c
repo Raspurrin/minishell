919215a3470332c85ca4178ea18415b6d5fb067b
Djaisin <vondetou@gmail.com>
2022-11-26 03:14:19 +0100

    Normed inout_files

--- srcs/execution/inout_files.c
+++ srcs/execution/inout_files.c
@@ -12,6 +12,17 @@
 
 #include "../../includes/minishell.h"
 
+int32_t	open_file(t_data *data, int32_t flags, char *name)
+{
+	int32_t	fd;
+
+	fd = open(name, flags, 0666);
+	if (fd == -1)
+		return (display_error(NODIR, join_err(name, NULL), \
+											data, NULL), 1);
+	return (fd);
+}
+
 /**
  * A heredoc is a file literal. It is a multi-line string taken from STDIN
  * ended by a delimiter. It is then treated as an input file.
@@ -25,15 +36,13 @@ static int32_t	here_doc(t_data *data, t_infile *lst)
 
 	(void)data;
 	line = ft_calloc(2, 1);
-	fd = open(lst->name, O_RDWR | O_CREAT | O_TRUNC, 0666);
-	if (fd == -1)
-		return (false);
+	fd = open_file(data, (O_RDWR | O_CREAT | O_TRUNC), lst->name);
 	while (ft_strncmp(line, lst->name, ft_strlen(lst->name) + 1) != 10)
 	{
 		free(line);
 		line = get_next_line(STDIN_FILENO);
 		if (line == NULL)
-			return (unlink(lst->name), 0); // instead of handling signals lol
+			return (unlink(lst->name), 0);
 		write(fd, line, ft_strlen(line));
 	}
 	free(line);
@@ -52,20 +61,9 @@ bool	infiles(t_data *data, t_group *group, t_fds *fds)
 	while (lst != NULL)
 	{
 		if (lst->here_doc == true)
-		{
 			fd = here_doc(data, lst);
-			if (!fd)
-				return (false);
-		}
 		else
-		{
-			fd = open(lst->name, O_RDONLY, 0666);
-			if (fd == -1)
-			{
-				fprintf(stderr, "opening file failed\n");
-				return (display_error(NODIR, join_err(NULL, NULL), data, NULL), 1);
-			}
-		}
+			fd = open_file(data, O_RDONLY, lst->name);
 		if (lst->next == NULL)
 		{
 			if (fds)
@@ -73,10 +71,7 @@ bool	infiles(t_data *data, t_group *group, t_fds *fds)
 				fds->infile = fd;
 				fds->std_in = dup(STDIN_FILENO);
 			}
-			if (dup2(fd, STDIN_FILENO) == -1)
-				printf("yooo dup2 failed\n");
-			close(fd);
-			return (true);
+			return (dup2(fd, STDIN_FILENO), close(fd), true);
 		}
 		close(fd);
 		lst = lst->next;
@@ -104,12 +99,9 @@ bool	outfiles(t_data *data, t_group *group, t_fds *fds)
 	while (lst != NULL)
 	{
 		if (lst->append == true)
-			fd = open(lst->name, O_RDWR | O_CREAT | O_APPEND, 0666);
+			fd = open_file(data, (O_RDWR | O_CREAT | O_APPEND), lst->name);
 		else
-			fd = open(lst->name, O_RDWR | O_CREAT | O_TRUNC, 0666);
-		if (fd == -1)
-			return (display_error(NODIR, join_err(lst->name, NULL), \
-													data, NULL), 1);
+			fd = open_file(data, (O_RDWR | O_CREAT | O_TRUNC), lst->name);
 		if (lst->next == NULL)
 		{
 			if (fds)
